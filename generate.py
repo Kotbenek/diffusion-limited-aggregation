451e635b7d67411335f6003d7eb575fc20c42374
Kotbenek <dominik.piatkowski@protonmail.com>
2023-01-19 22:22:09 +0100

    Implemented move function, updated draw function

--- generate.py
+++ generate.py
@@ -7,12 +7,22 @@ START_X = 0
 START_Y = 0
 STOP_X = 10
 STOP_Y = 10
+ION_MOVEMENT = 0.05
+
+def move(ions, active):
+    for i in range(ION_COUNT):
+        if active[i]:
+            for j in range(2):
+                ions[i][j] += random.uniform(-ION_MOVEMENT, ION_MOVEMENT)
+    ions[:,0] = np.clip(ions[:,0], START_X, STOP_X)
+    ions[:,1] = np.clip(ions[:,1], START_Y, STOP_Y)
 
 def draw(ions, active):
     plt.scatter(ions[:,0], ions[:,1], 1, c=active[:])
     plt.xlim(START_X, STOP_X)
     plt.ylim(START_Y, STOP_Y)
-    plt.show()
+    plt.pause(0.001)
+    plt.clf()
 
 if __name__ == "__main__":
     # Initialize
@@ -28,4 +38,6 @@ if __name__ == "__main__":
     ions[0][0] = (STOP_X - START_X) / 2
     ions[0][1] = (STOP_Y - START_Y) / 2
 
-    draw(ions, active)
+    for i in range(100):
+        move(ions, active)
+        draw(ions, active)
