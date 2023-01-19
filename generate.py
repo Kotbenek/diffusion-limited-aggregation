a688458f8281c09f5b27ad3008bcc6b20826eb01
Kotbenek <dominik.piatkowski@protonmail.com>
2023-01-19 22:06:41 +0100

    Implemented draw function

--- generate.py
+++ generate.py
@@ -1,11 +1,18 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 ION_COUNT = 10000
 START_X = 0
 START_Y = 0
 STOP_X = 10
 STOP_Y = 10
+
+def draw(ions, active):
+    plt.scatter(ions[:,0], ions[:,1], 1, c=active[:])
+    plt.xlim(START_X, STOP_X)
+    plt.ylim(START_Y, STOP_Y)
+    plt.show()
 
 if __name__ == "__main__":
     # Initialize
@@ -20,3 +27,5 @@ if __name__ == "__main__":
     active[0] = False
     ions[0][0] = (STOP_X - START_X) / 2
     ions[0][1] = (STOP_Y - START_Y) / 2
+
+    draw(ions, active)
