0895db3191fdffaac1acab1568171f667cc44a2a
Kotbenek <dominik.piatkowski@protonmail.com>
2023-01-19 21:53:12 +0100

    Prepare starting state of ions

--- generate.py
+++ generate.py
@@ -1,8 +1,22 @@
 import numpy as np
+import random
 
 ION_COUNT = 10000
+START_X = 0
+START_Y = 0
+STOP_X = 10
+STOP_Y = 10
 
 if __name__ == "__main__":
     # Initialize
     ions = np.empty((ION_COUNT, 2), dtype = float)
     active = [True for _ in range(ION_COUNT)]
+
+    # Put ions in random positions
+    for i in range(ION_COUNT):
+        ions[i] = [random.uniform(START_X, STOP_X), random.uniform(START_Y, STOP_Y)]
+
+    # Tree starting point
+    active[0] = False
+    ions[0][0] = (STOP_X - START_X) / 2
+    ions[0][1] = (STOP_Y - START_Y) / 2
